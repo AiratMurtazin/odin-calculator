4db8eb0f2994e6b44c3fe43ca5282f8928d054f0
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-10-30 16:22:00 +0300

    make it work with one operator and all numbers

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const multiply = document.getElementById('multiply')
 const divide = document.getElementById('divide')
 const equals = document.getElementById('equals')
 
+const keys = document.querySelectorAll('.keys')
 const one = document.getElementById('one')
 const two = document.getElementById('two')
 const three = document.getElementById('three')
@@ -20,7 +21,9 @@ const seven = document.getElementById('seven')
 const eight = document.getElementById('eight')
 const nine = document.getElementById('nine')
 const zero = document.getElementById('zero')
-
+let displayString = ''
+let displayArray = []
+let operator = ''
 // FUNCTIONS
 
 function addNumbers(a, b) {
@@ -53,3 +56,28 @@ function calculate(num1, operator, num2) {
 		display.value = divideNumbers(num1, num2)
 	}
 }
+
+keys.forEach(key => {
+	key.addEventListener('click', e => {
+		displayString += e.target.textContent
+		if (e.target.textContent !== '=') {
+			display.value += e.target.textContent
+		}
+	})
+})
+
+equals.addEventListener('click', () => {
+	console.log(displayString)
+	for (let i = 0; i <= displayString.length; i++) {
+		if (
+			displayString[i] === '+' ||
+			displayString[i] === '-' ||
+			displayString[i] === '*' ||
+			displayString[i] === '/'
+		) {
+			displayArray = displayString.split(displayString[i])
+			operator = displayString[i]
+		}
+	}
+	calculate(+displayArray[0], operator, +displayArray[1])
+})

b5d1807c5b65530045d7ea6cd04125608249e65f
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-10-31 11:31:10 +0300

    add clear btn logic

--- app.js
+++ app.js
@@ -4,30 +4,15 @@
 const display = document.getElementById('display')
 
 const clear = document.getElementById('clear')
-const plus = document.getElementById('add')
-const minus = document.getElementById('subtract')
-const multi = document.getElementById('multiply')
-const divid = document.getElementById('divide')
-const equals = document.getElementById('equals')
 
 const keys = document.querySelectorAll('.keys')
-const one = document.getElementById('one')
-const two = document.getElementById('two')
-const three = document.getElementById('three')
-const four = document.getElementById('four')
-const five = document.getElementById('five')
-const six = document.getElementById('six')
-const seven = document.getElementById('seven')
-const eight = document.getElementById('eight')
-const nine = document.getElementById('nine')
-const zero = document.getElementById('zero')
+
 const number = document.querySelector('.number')
 const oper = document.querySelector('.operator')
+
 let displayString = ''
 let displayArray = []
-let clicked = false
-// let operator = ''
-let onlyNumbersRegEx = /^\d+$/
+
 // FUNCTIONS
 
 function add(a, b) {
@@ -82,3 +67,9 @@ keys.forEach(key => {
 })
 
 equals.addEventListener('click', showAnswer)
+
+clear.addEventListener('click', () => {
+	displayString = ''
+	displayArray = []
+	display.value = ''
+})

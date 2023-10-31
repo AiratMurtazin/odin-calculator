428903f5cb2e3e67be831e242b6c935c2d354c99
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-10-31 10:15:46 +0300

    starting from scratch make basic functions

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 const display = document.getElementById('display')
 
 const clear = document.getElementById('clear')
-const add = document.getElementById('add')
-const subtract = document.getElementById('subtract')
-const multiply = document.getElementById('multiply')
-const divide = document.getElementById('divide')
+const plus = document.getElementById('add')
+const minus = document.getElementById('subtract')
+const multi = document.getElementById('multiply')
+const divid = document.getElementById('divide')
 const equals = document.getElementById('equals')
 
 const keys = document.querySelectorAll('.keys')
@@ -23,61 +23,37 @@ const nine = document.getElementById('nine')
 const zero = document.getElementById('zero')
 let displayString = ''
 let displayArray = []
-let operator = ''
+// let operator = ''
+let onlyNumbersRegEx = /^\d+$/
 // FUNCTIONS
 
-function addNumbers(a, b) {
+function add(a, b) {
 	return a + b
 }
-
-function subtractNumbers(a, b) {
+function subtract(a, b) {
 	return a - b
 }
-
-function multiplyNumbers(a, b) {
+function multiply(a, b) {
 	return a * b
 }
-
-function divideNumbers(a, b) {
+function divide(a, b) {
 	return a / b
 }
 
-function calculate(num1, operator, num2) {
-	if (operator === '+') {
-		display.value = addNumbers(num1, num2)
-	}
-	if (operator === '-') {
-		display.value = subtractNumbers(num1, num2)
-	}
-	if (operator === '*') {
-		display.value = multiplyNumbers(num1, num2)
-	}
-	if (operator === '/') {
-		display.value = divideNumbers(num1, num2)
+function operator(num1, operator, num2) {
+	switch (operator) {
+		case '+':
+			return add(num1, num2)
+			break
+		case '-':
+			return subtract(num1, num2)
+			break
+		case '*':
+			return multiply(num1, num2)
+			break
+		case '/':
+			return divide(num1, num2)
+		default:
+			console.log('Do your calculations!')
 	}
 }
-
-keys.forEach(key => {
-	key.addEventListener('click', e => {
-		displayString += e.target.textContent
-		if (e.target.textContent !== '=') {
-			display.value += e.target.textContent
-		}
-	})
-})
-
-equals.addEventListener('click', () => {
-	console.log(displayString)
-	for (let i = 0; i <= displayString.length; i++) {
-		if (
-			displayString[i] === '+' ||
-			displayString[i] === '-' ||
-			displayString[i] === '*' ||
-			displayString[i] === '/'
-		) {
-			displayArray = displayString.split(displayString[i])
-			operator = displayString[i]
-		}
-	}
-	calculate(+displayArray[0], operator, +displayArray[1])
-})
